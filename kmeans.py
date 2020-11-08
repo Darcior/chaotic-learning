008e78bfa6d8bd031cffea17f949063dece3b5c4
aditya-petety <73835819+aditya-petety@users.noreply.github.com>
2020-11-08 14:47:23 +0530

    Add files via upload

---
+++ kmeans.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 14:31:21 2020
+
+@author: admin
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+matrix = np.loadtxt('param.txt', dtype = 'f')
+embd = np.zeros( (2048, 5) )
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+target = np.loadtxt('data.txt', dtype = 'f')
+for i in range(2048):
+    if target[i] > 0:
+        target[i]=1
+    else:
+        target[i]=0    
+
+X_train , X_test, y_train,  y_test = train_test_split(embd, target, random_state=44, train_size = 0.2)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred, zero_division=1))
---
+++ logistic_Regression.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 14:20:56 2020
+
+@author: admin
+"""
+import numpy as np
+matrix = np.loadtxt('param.txt', dtype = 'f')
+embd = np.zeros( (2048, 5) )
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+target = np.loadtxt('data.txt', dtype = 'f')
+for i in range(2048):
+    if target[i] > 0:
+        target[i]=1
+    else:
+        target[i]=0    
+
+X_train , X_test, y_train,  y_test = train_test_split(embd, target, random_state=44, train_size = 0.2)
+
+clf = LogisticRegression().fit(X_train, y_train)
+clf2 = clf.predict(X_test)
+from sklearn.metrics import f1_score
+print(f1_score(y_test, clf2, average='micro'))
+clf4 = LogisticRegression(max_iter = 2000).fit(X_train, y_train)
+clf5 = clf4.predict(X_test)
+print(f1_score(y_test, clf5, average='micro'))
